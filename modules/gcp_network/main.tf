34d50bd6322fbf1424406bfeeffd201a39538e12
baremetal245 <aditya24g@protonmail.com>
2026-09-09 16:44:29 +0530

    wip: x090926

--- modules/gcp_network/main.tf
+++ modules/gcp_network/main.tf
@@ -7,7 +7,7 @@ resource "google_compute_network" "vpc_network" {
 
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.network_name}-subnet"
+  name          = var.subnet_name
   ip_cidr_range = var.subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc_network.id
--- modules/gcp_network/variables.tf
+++ modules/gcp_network/variables.tf
@@ -21,3 +21,9 @@ type            = string
 default         = "10.0.1.0/24"
 description     = "Primary subnet CIDR range"
 }
+
+
+variable "subnet_name" {
+  type        = string
+  description = "Name of the subnet"
+}
